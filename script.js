25a7978433d80ceb50201efdcd577d2182bc08da
Jamie Kook <kookjc6@gmail.com>
2019-11-13 22:01:11 -0600

    moved hour title down by adding margin to hour div

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let timesArr= ["9AM","10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 for (i=1; i<timesArr.length; i++){
     let newTimeBlockEL= $("#9am").clone();
     newTimeBlockEL.attr("id", timesArr[i]); 
-    newTimeBlockEL.children(".row").attr("style", "whiteSpace: preWrap"); 
+    newTimeBlockEL.children(".row").attr("style", "white-space: pre-Wrap"); 
     newTimeBlockEL.children(".row").children(".hour").text(timesArr[i]); 
     newTimeBlockEL.appendTo(".container"); 
 
