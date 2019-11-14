a69379ccb0ab7db419a55341e65702b8e9bb6e6a
Jamie Kook <kookjc6@gmail.com>
2019-11-13 21:54:58 -0600

    used jquery to clone and populate the remaining time-blocks

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+//Element names
+
 let timeBlockContainer = $(".container"); 
 let todaysDateEl= $("#currentDay"); 
 
-//Code to generate time blocks
 
+//Code generating the current date 
 todaysDateEl.text(moment().format("dddd, MMMM Do")); 
 
+//Code to generate time blocks
+let timesArr= ["9AM","10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]; 
+
+for (i=1; i<timesArr.length; i++){
+    let newTimeBlockEL= $("#9am").clone();
+    newTimeBlockEL.attr("id", timesArr[i]); 
+    newTimeBlockEL.children(".row").attr("style", "whiteSpace: preWrap"); 
+    newTimeBlockEL.children(".row").children(".hour").text(timesArr[i]); 
+    newTimeBlockEL.appendTo(".container"); 
+
+}; 
